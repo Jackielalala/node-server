bd8633eaed84ac1a9713c521e2e987a09df1b178
Wong <554586805@qq.com>
2018-11-10 11:24:55 +0800

    Update b.js

--- static/b.js
+++ static/b.js
@@ -1,5 +1,5 @@
 var xhr=new XMLHttpRequest();
-xhr.open('GET','/getWeather',true);
+xhr.open('GET','/getWeather',true);//请求以GET的方式发送，发送到url对应的服务器，以异步的方式
 xhr.send();
 xhr.onload=function(){
     console.log(JSON.parse(xhr.responseText));
